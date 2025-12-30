fec6a4112853cf048138b40f7535d1edc8501866
Elijah Ducote <validpupflower@gmail.com>
2025-12-30 02:29:45 -0800

    New commit

--- nav/shared/index.js
+++ nav/shared/index.js
@@ -2,8 +2,6 @@ import {paths as sectionIds} from "../../sitemap.json";
 import {Loader} from "vanjs-feather";
 import van from "vanjs-core";
 
-eruda.init();
-
 let leavingPage = false;
 
 function initAnimations() {
